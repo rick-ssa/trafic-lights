e1278b5a3923feedd9d1f2f9d47f612b3699e7d3
rick-ssa <rick_ssa@hotmail.com>
2024-06-27 01:55:43 -0300

    created the light component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
 import "./App.css"
+import Light from "./components/Lights/BasicLight"
 
 function App() {
-    return <div className="App"></div>
+    const [on, setOn] = useState(false)
+    return (
+        <div className="App">
+            <Light on={on} color="#f00" size={75} />
+            <p />
+            <Light on={on} color="#ff0" size={75} />
+            <p />
+            <Light on={on} color="#0f0" size={75} />
+            <p />
+            <button onClick={() => setOn(!on)}>
+                {on ? "desligar" : "ligar"}
+            </button>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Lights/BasicLight/index.tsx
@@ -0,0 +1,60 @@
+import "./styles.css"
+
+interface BasicLightProps {
+    size: number
+    on: boolean
+    color: string
+}
+
+const BasicLight = ({ size, on, color }: BasicLightProps) => {
+    // make the shine bigger than the Basiclight object
+    const BasiclightShineSize = size * 1.1
+
+    // center the shine with the Basiclight object
+    const BasiclightShinePosition = (-1 * BasiclightShineSize + size) / 2
+
+    return (
+        <div
+            className="basiclight-container"
+            style={{
+                width: size + "px",
+                height: size + "px",
+            }}
+        >
+            {/* make the Basiclight look like it is shine*/}
+            <span
+                className="basiclight basiclight-shine"
+                style={{
+                    width: BasiclightShineSize + "px",
+                    height: BasiclightShineSize + "px",
+                    left: BasiclightShinePosition + "px",
+                    top: BasiclightShinePosition + "px",
+                    backgroundColor: color,
+                    visibility: on ? "visible" : "hidden",
+                }}
+            />
+
+            {/* show the Basiclight */}
+            <span
+                className="basiclight"
+                style={{
+                    width: size + "px",
+                    height: size + "px",
+                    backgroundColor: color,
+                }}
+            />
+
+            {/* covers the Basiclight when on = false */}
+            <span
+                className="basiclight basiclight-off"
+                style={{
+                    width: size + "px",
+                    height: size + "px",
+                    visibility: !on ? "visible" : "hidden",
+                }}
+            />
+        </div>
+    )
+}
+
+export default BasicLight
