381d69721c13dba22c09d0acd48a6de63ce0bd5d
rick-ssa <rick_ssa@hotmail.com>
2024-07-03 21:48:05 -0300

    inserted comment in the function

--- src/utils/getLightColorOff.ts
+++ src/utils/getLightColorOff.ts
@@ -1,12 +1,15 @@
 const getLightColorOff = (color: string) => {
     const hexaColorPattern = /^(#[0-9a-fA-F]{3}|#[0-9a-fA-F]{6})$/
 
+    //! only allowed hexa colors like #f52 or #ff5522
     if (!hexaColorPattern.test(color)) {
         throw new Error(
             "A função getLightColorOff só aceita cores em formatos hexadecimais"
         )
     }
 
+    //* return 1/3 of the value of the componente color
+    //* 1/3 red, 1/3 green and 1/3 blue
     if (color.length === 4) {
         return color.replace(/\w/g, (match) => {
             const num = parseInt(match + match, 16)
